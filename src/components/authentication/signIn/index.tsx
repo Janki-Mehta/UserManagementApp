f7b5b945a30c230bf3b2dd35480aadabdf33ac75
Janki-Mehta <janu.shivay.01@gmail.com>
2026-03-15 15:51:06 +0530

    Initial project setup

---
+++ src/components/authentication/signIn/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SignInComponent = () => {
+  return (
+    <View>
+      <Text>SignInComponent</Text>
+    </View>
+  )
+}
+
+export default SignInComponent
---
+++ src/containers/authentication/signIn/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SignInContainer = () => {
+  return (
+    <View>
+      <Text>SignInContainer</Text>
+    </View>
+  )
+}
+
+export default SignInContainer
